6e117f0f24d015ebf9396a8538e70d9738c42fe3
Daniel Theng <thengdaniel@gmail.com>
2020-07-11 12:10:37 -0700

    run_analysis tidys up the data into final document Final_Dataset.txt

---
+++ run_analysis.R
@@ -0,0 +1,7 @@
+#Getting and Cleaning Data Assignment
+    #Download the Data
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset
+            .zip"
+download.file(fileURL, 
+              destfile = "/Users/danieltheng/Documents/Daniel Theng/R-Coursera/Getting and Cleaning Data/Getting-and-Cleaning-Data-Assignment", 
+              )
